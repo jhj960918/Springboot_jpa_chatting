451850ea51c62cbb10cd0177fd5bc5a549f8947b
jhj960918 <65895403+jhj960918@users.noreply.github.com>
2020-11-09 22:34:13 +0900

    채팅방 인원수 퇴장 표시

--- src/main/java/com/victolee/board/controller/ChatRoomController.java
+++ src/main/java/com/victolee/board/controller/ChatRoomController.java
@@ -28,8 +28,11 @@ public class ChatRoomController {
     @GetMapping("/rooms")
     @ResponseBody
     public List<ChatRoom> room() {
-        return chatRoomRepository.findAllRoom();
+        List<ChatRoom> chatRooms = chatRoomRepository.findAllRoom();
+        chatRooms.stream().forEach(room -> room.setUserCount(chatRoomRepository.getUserCount(room.getRoomId())));
+        return chatRooms;
     }
+
 
     @PostMapping("/room")
     @ResponseBody
